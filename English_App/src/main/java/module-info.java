6414c60fbcb59217b2c15cd81cf174a0e648415e
Duong Binh Minh <minimum.195422@gmail.com>
2024-09-21 03:58:44 +0700

    Delete project
    Delete demo project

--- English_App/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module uet.student.project.english_app {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens uet.student.project.english_app to javafx.fxml;
-    exports uet.student.project.english_app;
-}
