f15633bb9069868d3ae4f397cae2891aea2a633d
Duong Binh Minh <minimum.195422@gmail.com>
2024-09-30 04:33:07 +0700

    ID012 - HomePage
    Delete current homepage, change to a new design style

--- English-App/src/main/java/project/englishapp/Controllers/HomeController.java
+++ English-App/src/main/java/project/englishapp/Controllers/HomeController.java
@@ -1,17 +1,14 @@
 package project.englishapp.Controllers;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.input.ScrollEvent;
-import javafx.scene.layout.HBox;
+import javafx.fxml.Initializable;
 
-public class HomeController {
-    @FXML
-    public ScrollPane Home_HomeScroll;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    public void Home_HomeScroll(ScrollEvent scrollEvent) {
-        double y_pos = scrollEvent.getDeltaY();
-        double height = Home_HomeScroll.getContent().getBoundsInLocal().getHeight();
-        Home_HomeScroll.setVvalue(Home_HomeScroll.getVvalue() - y_pos/height);
+public class HomeController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
     }
 }
